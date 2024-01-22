770b5e311f60863ce1e1d0324de1f436fb25d873
Saharbabiab <117641327+Saharbabiab@users.noreply.github.com>
2024-01-22 20:12:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,20 +2,18 @@ const app = require("./app");
 const MongoDatabase = require("./app/DAL/db");
 
 const run = async () => {
-    try {
-        const port = process.env.PORT || "3000";
-        await MongoDatabase.instance().connect();
-        app.listen(port, () => console.log(`Listening on port: ${port}`));
-    }
-    catch (err) {
-        console.log(`FAILED TO START: ${err}`)
-    }
-}
+  try {
+    const port = process.env.PORT || "27017";
+    await MongoDatabase.instance().connect();
+    app.listen(port, () => console.log(`Listening on port: ${port}`));
+  } catch (err) {
+    console.log(`FAILED TO START: ${err}`);
+  }
+};
 
 run();
 
-process.on('SIGINT', async () => {
-    await MongoDatabase.instance().disconnect();
-    process.exit(0);
+process.on("SIGINT", async () => {
+  await MongoDatabase.instance().disconnect();
+  process.exit(0);
 });
-
